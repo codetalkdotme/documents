c0b9ac9ae3b489368487be0e79c58c615f53934e
guobxu <gbxu@outlook.com>
2017-12-06 22:30:23 +0800

    change

--- auth/design/export/authdb.sql
+++ auth/design/export/authdb.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2017/12/4 20:59:07                           */
+/* Created on:     2017/12/6 17:04:08                           */
 /*==============================================================*/
 
 
@@ -15,12 +15,14 @@ create table auth_users
    user_login           varchar(30) not null comment '用户登录名',
    user_name            varchar(200) comment '用户名: 真实姓名或者昵称',
    user_profile         varchar(200) comment '用户头像url',
+   user_passwd          varchar(50) not null comment '用户密码',
    user_signature       varchar(300) comment '用户个性签名',
    user_mobile          varchar(30) comment '用户手机号码',
    mobile_verified      int not null default 0 comment '手机号码是否验证 0 否 1 是',
    user_mail            varchar(100) comment '用户邮箱',
    mail_verified        int not null default 0 comment '邮箱是否验证 0 否 1 是',
    user_disabled        int not null default 0 comment '用户是否禁用 0 否 1 是',
+   disable_reason       varchar(200) comment '禁用用户的原因',
    attr1                varchar(300),
    attr2                varchar(300),
    attr3                varchar(300),
